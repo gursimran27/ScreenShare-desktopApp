b4cfc884c92af3d4a928fcf80532e7e252355eba
Gursimran27 <singhgursimran165@gmail.com>
2024-11-23 13:52:49 +0530

    first

---
+++ client/main.js
@@ -0,0 +1,75 @@
+// main.js
+
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { v4: uuidv4 } = require("uuid");
+const screenshot = require("screenshot-desktop");
+
+var socket = require("socket.io-client")("http://127.0.0.1:5000");
+var interval;
+let uuid;
+
+const createWindow = () => {
+  // Create the browser window.
+  const win = new BrowserWindow({
+    width: 500,
+    height: 150,
+    webPreferences: {
+      nodeIntegration: true,// Required for `require('electron')` in renderer
+      contextIsolation: false,// Allows access to Electron APIs
+    },
+  });
+
+  //  to remove menu items
+  win.removeMenu();
+
+  // and load the index.html of the app.
+  win.loadFile("./index.html");
+
+  // Open the DevTools.
+  //   win.webContents.openDevTools()
+
+};
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
+
+ipcMain.on("start-share", function (event, arg) {
+  uuid =  uuidv4();
+  socket.emit("join-message", uuid);
+  event.reply("uuid", uuid);
+
+  interval = setInterval(function () {
+    screenshot().then((img) => {
+      var imgStr = Buffer.from(img).toString("base64");
+
+      var obj = {};
+      obj.room = uuid;
+      obj.image = imgStr;
+
+      socket.emit("screen-data", JSON.stringify(obj));
+    });
+  }, 500);
+});
+
+ipcMain.on("stop-share", function (event, arg) {
+  socket.emit("end-session",uuid );
+  clearInterval(interval);
+});
---
+++ server/index.js
@@ -0,0 +1,76 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+const io = require("socket.io")(http, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+app.get('/view', (req, res) => {
+    res.sendFile(__dirname + '/display.html');
+})
+
+io.on('connection', (socket)=> {
+
+    console.log("New client connected");
+
+    socket.on("join-message", (roomId) => {
+        socket.join(roomId);
+        console.log("User joined in a room : " + roomId);
+    })
+
+    socket.on("screen-data", function(data) {
+        data = JSON.parse(data);
+        var room = data.room;
+        var imgStr = data.image;
+        socket.broadcast.to(room).emit('screen-data', imgStr);
+    })
+
+    socket.on("end-session", function(uuid) {
+        socket.broadcast.to(uuid).emit('end-session');
+    })
+})
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 5000;
+http.listen(server_port, () => {
+    console.log("Started on : "+ server_port);
+})
+
+app.get('/', (req, res) => {
+    return res.send(`
+        <h1>Visit the /view path</h1>
+        <button class="styled-btn" onclick="window.location.href='/view'">Go to View Path</button>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                text-align: center;
+                margin-top: 20%;
+                background: linear-gradient(45deg, #7b0909, #6771b8b8);
+                color: white;
+                height: 100vh;
+                width: 100vw;
+                overflow: hidden;
+            }
+
+            .styled-btn {
+                padding: 15px 30px;
+                font-size: 18px;
+                background-color: #4CAF50;
+                color: white;
+                border: none;
+                border-radius: 8px;
+                cursor: pointer;
+                transition: background-color 0.3s, transform 0.2s;
+            }
+
+            .styled-btn:hover {
+                background-color: #45a049;
+                transform: scale(1.05);
+            }
+
+            .styled-btn:active {
+                background-color: #3e8e41;
+            }
+        </style>
+    `);
+})
